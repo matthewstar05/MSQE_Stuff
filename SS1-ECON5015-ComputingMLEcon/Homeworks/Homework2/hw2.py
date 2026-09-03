093c8868aeb4837f2f2eb0e65d7fc55d9bee82ed
matthewstar05 <matthewsuban@gmail.com>
2026-09-03 13:16:58 -0700

    delta

---
+++ SS1-ECON5015-ComputingMLEcon/Homeworks/Homework2/hw2.py
@@ -0,0 +1,15 @@
+"""
+ECON 5015: Homework 2
+Matthew Suban
+@matthewsuban
+"""
+import math
+
+a = []
+for i in range(1,101):
+    a.append(i)
+print(a) # l1 = positive integers from 1 to 100
+
+b = []
+
+
